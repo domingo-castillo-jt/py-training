aa523723ac31b614ea57fc2f52e017a7301aa4e1
domingo-castillo-jt <domingo.castillo@jobandtalent.com>
2022-06-06 08:37:44 +0200

    create collection

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
  
@@ -8,6 +8,7 @@ import rollbar.contrib.flask
 from flask import got_request_exception
 import requests
 
+write_token = "6408eab7199940528fd17e193b7c7f5e"
 
 @app.before_first_request
 def init_rollbar():
@@ -35,9 +36,17 @@ def get_items():
     x = requests.get("http://api.rollbar.com/api/1/items", headers={"X-Rollbar-Access-Token":"29b05bd130e14b27822d17787597ce72"})
     return x.text
 
-@app.route('/item/<int:id>')
+@app.route('/items/<int:id>')
 def get_item(id):
     x = requests.get(f"http://api.rollbar.com/api/1/item/{id}", headers={"X-Rollbar-Access-Token":"29b05bd130e14b27822d17787597ce72"})
-
+    return x.text
+    
+@app.route('/items', methods=['POST'])
+def post_item():
+    x = requests.post("http://api.rollbar.com/api/1/item/", data=request.data, headers={"X-Rollbar-Access-Token":"3c4fed617abb463caab073397f26507a"})
     return x.text
 
+@app.route('/items/<int:id>', methods=['PUT', 'PATCH'])
+def put_item(id):
+    x = requests.patch(f"http://api.rollbar.com/api/1/item/{id}", data=request.data, headers={"X-Rollbar-Access-Token":"6408eab7199940528fd17e193b7c7f5e"})
+    return x.text
