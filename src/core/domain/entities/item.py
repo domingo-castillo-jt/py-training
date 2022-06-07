3f5fb2abe97846611fbd78414954c51f98e79c90
domingo-castillo-jt <domingo.castillo@jobandtalent.com>
2022-06-07 13:27:05 +0200

    change to Optional

--- src/core/domain/entities/item.py
+++ src/core/domain/entities/item.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Union
+from typing import Optional
 
 
 class Item(BaseModel):
@@ -16,14 +16,14 @@ class Item(BaseModel):
     activating_occurrence_id: int
     last_activated_timestamp: int
     last_resolved_timestamp: int
-    last_muted_timestamp: Union[int, None]
+    last_muted_timestamp: Optional[int]
     last_occurrence_id: int
     last_occurrence_timestamp: int
     total_occurrences: int
     last_modified_by: int
     status: str
     level: str
-    integrations_data: Union[int, None]
-    assigned_user_id: Union[int, None]
-    group_item_id: Union[int, None]
+    integrations_data: Optional[int]
+    assigned_user_id: Optional[int]
+    group_item_id: Optional[int]
     group_status: int
